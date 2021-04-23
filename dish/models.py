a28070eb1c36531e1fb3efacbbf31d7f6919706a
Amankaium <kaium@list.ru>
2021-04-23 21:04:24 +0600

    added dish model

--- dish/models.py
+++ dish/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'блюдо'
+        verbose_name_plural = 'Блюда'
+
+    def __str__(self):
+        return self.name
