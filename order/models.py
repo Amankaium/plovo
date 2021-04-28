50ec16dddd9b7f53ae532baa7b4669070e9d987c
Amankaium <kaium@list.ru>
2021-04-28 19:51:44 +0600

    add order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from dish.models import Dish
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+
+    dish = models.ForeignKey(
+        to=Dish,
+        on_delete=models.CASCADE
+    )
+
+    location = models.CharField(max_length=500)
+    phone = models.CharField(max_length=20)
+    discount = models.BooleanField(default=False)
+    discount_sum = models.IntegerField(default=0)
+    status = models.IntegerField(default=0, choices=(
+        (0, 'Accepted'),
+        (1, 'Completed'),
+        (2, 'Canceled'),
+    ))
+
+    def __str__(self):
+        return self.phone
+    
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'Заказы'
