4ff3f824dc897a389bac3d452b6cb2ffe6f278c1
Amankaium <kaium@list.ru>
2021-04-28 21:13:48 +0600

    add order list and create

--- order/views.py
+++ order/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import OrderSerializer
+from .models import Order
+
+class OrderListCreateView(APIView):
+    def get(self, request, *args, **kwargs):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(data=serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = OrderSerializer(data=request.POST)
+        if serializer.is_valid():
+            order_object = serializer.save()
+            json_serializer = OrderSerializer(instance=order_object)
+            return Response(data=json_serializer.data, status=201)
+        return Response(data=serializer.errors)
+
+
+class OrderView(APIView):
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def delete(self, request, *args, **kwargs):
+        pass
