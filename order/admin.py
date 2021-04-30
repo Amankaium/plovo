e04d37d6c9f16a6e12b3bd9138365e60404f8057
Amankaium <kaium@list.ru>
2021-04-30 20:51:16 +0600

    made order list display the tuple

--- order/admin.py
+++ order/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Order
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['phone', 'dish', 'location', 'status']
+    list_display = ('phone', 'dish', 'location', 'status')
     list_editable = ("status",)
 
 admin.site.register(Order, OrderAdmin)
