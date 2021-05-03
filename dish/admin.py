ac0584da40b22f7d424e3292156c21e36d10dba6
Amankaium <kaium@list.ru>
2021-05-03 20:53:12 +0600

    add dish admin list

--- dish/admin.py
+++ dish/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Dish
 
-# Register your models here.
-admin.site.register(Dish)
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ['name']
+
+
+admin.site.register(Dish, DishAdmin)
