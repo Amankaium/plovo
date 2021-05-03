f2ade1f1298a6c81a16c39729b3ab8e4d241ae02
Islam <78363252+aibekovislam@users.noreply.github.com>
2021-05-03 19:32:26 +0300

    bla bla

--- order/admin.py
+++ order/admin.py
@@ -4,5 +4,7 @@ from .models import Order
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('phone', 'dish', 'location', 'status')
     list_editable = ("status",)
+    search_fields = ('dish__name', 'locations')
+    list_filter = ('status',)
 
 admin.site.register(Order, OrderAdmin)
