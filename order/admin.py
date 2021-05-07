5c21f57a341a2cb07048b348bdaab82405a3b227
Amankaium <kaium@list.ru>
2021-05-07 21:00:32 +0600

    added dish inline

--- order/admin.py
+++ order/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, AdditionDish
+
+class AdditionDishInline(admin.TabularInline):
+    model = AdditionDish
+    extra = 0
+
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('phone', 'dish', 'location', 'status')
     list_editable = ("status",)
     search_fields = ('dish__name', 'location')
     list_filter = ('status',)
+    inlines = [AdditionDishInline]
 
 admin.site.register(Order, OrderAdmin)
+
+
+class AdditionDishAdmin(admin.ModelAdmin):
+    list_display = ('dish', 'order', 'created_at', 'updated_at')
+    fields = ('dish', 'order', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(AdditionDish, AdditionDishAdmin)
