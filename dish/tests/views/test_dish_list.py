5e0541e26911ded3d49c5eec77c0b57475be0a8d
Amankaium <kaium@list.ru>
2021-05-07 19:33:33 +0600

    added test create dish

--- dish/tests/views/test_dish_list.py
+++ dish/tests/views/test_dish_list.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
+from rest_framework import status
 
 from dish.tests.factories import DishFactory
 
@@ -13,7 +14,7 @@ class DishListAPITestCase(APITestCase):
         dish_2 = DishFactory()
 
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, list)
 
         expected_data_1 = {
@@ -33,10 +34,57 @@ class DishListAPITestCase(APITestCase):
     
     def test_dish_list_other_methods_should_return_405(self):
         response = self.client.post(self.url)
-        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
         response = self.client.put(self.url)
-        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
         response = self.client.delete(self.url)
-        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class TestDishCreateAPITestCase(APITestCase):
+    def setUp(self):
+        self.data = {
+            'name': 'Тестовое блюдо от шефа',
+            'price': 150
+        }
+
+        self.url = reverse('create-dish')
+    
+    def test_create_dish_success(self):
+        response = self.client.post(
+            path=self.url,
+            data=self.data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        expected_data = {'message': 'Блюдо успешно добавлено'}
+        self.assertEqual(response.data, expected_data)
+    
+    def test_create_dish_via_get_405(self):
+        response = self.client.get(
+            path=self.url,
+            data=self.data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        expected_data = {"detail": "Method \"GET\" not allowed."}
+        self.assertEqual(response.data, expected_data)
+    
+
+    def test_create_dish_without_data_400(self):
+        response = self.client.post(
+            path=self.url,
+            data={"name": "Test"}
+        )
+
+        expected_data = {
+            "price": [
+                "This field is required."
+            ]
+        }
+
+        self.assertEqual(response.data, expected_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
