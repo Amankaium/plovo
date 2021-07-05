a9f0608757a7719eb0a34b71dac375ae91e49ffb
Amankaium <kaium@list.ru>
2021-07-05 20:20:01 +0600

    fix dish admin

--- dish/admin.py
+++ dish/admin.py
@@ -6,7 +6,6 @@ class DishAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
     search_fields = ['name']
     fields = ('name', 'price', 'calories')
-    readonly_fields = ['name']
 
 
 admin.site.register(Dish, DishAdmin)
